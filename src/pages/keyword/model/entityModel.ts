01100c85c1d4c4b8cc2acd1f31a5f59eb2b151de
clothme-io <clothme2019@outlook.com>
2023-08-14 23:45:15 -0700

    rebase with main

---
+++ src/pages/keyword/model/entityModel.ts
@@ -0,0 +1,19 @@
+export interface EntityModel {
+    id: string;
+    url: string;
+    created_at: string;
+    updated_at: string;
+    name: string;
+    created_by: string;
+    account_id: string
+    project_id: string;
+    entity_number: number;
+    type: string;
+    details: {
+      related_search: any,
+      suggestion: any
+      related_queries: any,
+      keywords: any
+    };
+  
+  }
