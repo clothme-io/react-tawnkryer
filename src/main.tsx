39dd68ea9f5fbe3f25d89e67d7ad41c583986f86
clothme <57736523+clothme-io@users.noreply.github.com>
2023-11-02 13:16:15 -0700

    removed maine and tiptap

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { MantineProvider } from '@mantine/core';
 import { App } from './app/App';
 import { AuthProvider } from './hooks/useAuth';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <AuthProvider>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <App />
-      </MantineProvider>
+      <App />
     </AuthProvider>
   </BrowserRouter>
 );
--- src/pages/writer/components/WriterDataComponent.tsx
+++ src/pages/writer/components/WriterDataComponent.tsx
@@ -1,9 +1,7 @@
-import { RichTextEditorComponent } from '../../../components/ui/richTextEditorComponent';
-
 export function WriterDataComponent() {
   return (
     <div className="pt-8 px-4" style={{ height: '100vh' }}>
-      <RichTextEditorComponent />
+      <p>Here</p>
     </div>
   );
 }
